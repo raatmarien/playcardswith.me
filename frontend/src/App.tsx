8d9cfeecdaa7ceba22d20e9736072507820618a4
Jochem Raat <jochem@invulns.nl>
2020-03-27 18:01:49 +0100

    Make the app stateful

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,32 +2,53 @@ import React from 'react';
 import './App.css';
 import * as Colyseus from "colyseus.js";
 
-function App() {
-    let client = new Colyseus.Client("ws://localhost:3000");
-    client.joinOrCreate("room").then(room => {
-        console.log("joined");
-        room.onStateChange.once(function(state) {
-            console.log("initial room state:", state);
+type Props = {};
+
+type State = {
+    numberOfDecks: number;
+};
+
+export default class App extends React.Component<Props, State> {
+    public client: any;
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            numberOfDecks: 0
+        };
+
+    }
+
+    public componentDidMount() {
+        let client = new Colyseus.Client("ws://localhost:3000");
+        client.joinOrCreate("room").then(room => {
+            console.log("joined");
+            room.onStateChange.once(this.updateRoomState.bind(this));
+
+            // new room state
+            room.onStateChange(this.updateRoomState.bind(this));
+
+            // listen to patches coming from the server
+            room.onMessage(function(message) {
+                console.log("New message", message);
+            });
+
         });
+    }
 
-        // new room state
-        room.onStateChange(function(state) {
-            // this signal is triggered on each patch
-            console.log("New state:", state);
+    private updateRoomState(state: any) {
+        console.log("room state:", state);
+        this.setState({
+            numberOfDecks: state.decks.length,
         });
-
-        // listen to patches coming from the server
-        room.onMessage(function(message) {
-            console.log("New message", message);
-        });
-    });
+    }
 
 
-    return (
-        <div className="App">
-            Hoi!
-        </div>
-    );
+    public render() {
+        return (
+            <div className="App">
+                {this.state.numberOfDecks}
+            </div>
+        );
+    }
 }
-
-export default App;
