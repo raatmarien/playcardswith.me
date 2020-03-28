d506c4dc50898f4717f7542b8abba636659c4496
Jochem Raat <jochem@invulns.nl>
2020-03-28 14:31:25 +0100

    Fix glitchy dragging behaviour
    Dragging used to be glitchy because of the inconsistency between the
    local card location and the one from the server. The servers location
    of course updates slower than then the local one. Therefore, the card
    would slowly drift off track.
    
    This is fixed by:
    
    1. Moving the draggable component within the cardcomponent (this makes
    more sense anyway, separation of concerns)
    2. Add a 'dragging' boolean to the state of each card. If the card is
    being dragged, it will use its own internal state to decide its
    location. Once it is done dragging, it will use the position from the
    server again.

--- frontend/src/CardComponent.tsx
+++ frontend/src/CardComponent.tsx
@@ -1,12 +1,80 @@
 import React from 'react';
 import {LocatedCard} from "cards-library";
 import "./CardComponent.css";
+import Draggable, { DraggableData } from "react-draggable";
 
 type Props = {
     locatedCard: LocatedCard,
+    sendMessage: (msg: any) => void,
 };
 
-export default class CardComponent extends React.Component<Props, {}> {
+type State = {
+    dragging: boolean,
+};
+
+
+export default class CardComponent extends React.Component<Props, State> {
+    private startX: number = -1;
+    private startY: number = -1;
+
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            dragging: false,
+        };
+    }
+
+
+    private onCardClick(id: number) {
+        this.props.sendMessage({
+            messageType: "card_turn",
+            cardId: id,
+        });
+    }
+
+    private countAsClick(data: DraggableData) {
+        let difX = this.startX - data.x;
+        let difY = this.startY - data.y;
+        let distSq = difX * difX + difY * difY;
+        return distSq < 100;
+    }
+
+    private onDragStart(locatedCard: LocatedCard, data: DraggableData) {
+        this.setState({
+            dragging: true,
+        });
+
+        this.startX = data.x;
+        this.startY = data.y;
+        this.props.sendMessage({
+            messageType: "card_drag",
+            cardId: locatedCard.card.id,
+            cardX: data.x,
+            cardY: data.y,
+        });
+    }
+
+    private onDragMove(locatedCard: LocatedCard, data: DraggableData) {
+        this.props.sendMessage({
+            messageType: "card_drag",
+            cardId: locatedCard.card.id,
+            cardX: data.x,
+            cardY: data.y,
+        });
+    }
+
+    private onDragStop(locatedCard: LocatedCard, data: DraggableData) {
+        this.setState({
+            dragging: false,
+        });
+
+        if (this.countAsClick(data)) {
+            this.onCardClick(locatedCard.card.id);
+        }
+    }
+
+
     public render() {
         let className = "card ";
         if (this.props.locatedCard.card.open) {
@@ -14,13 +82,29 @@ export default class CardComponent extends React.Component<Props, {}> {
         } else {
             className += "card-closed";
         }
+        let locatedCard = this.props.locatedCard;
 
         return (
-            <div className={className}>
-                {this.props.locatedCard.card.open
-                ? this.props.locatedCard.card.name
-                : ""}
-            </div>
+            <Draggable
+                key={locatedCard.card.id}
+                position={
+                this.state.dragging ? undefined
+                : { x: locatedCard.location.x,
+                    y: locatedCard.location.y}}
+                onStart={(e, data) =>
+                    this.onDragStart(locatedCard, data)}
+                onDrag={(e, data) =>
+                    this.onDragMove(locatedCard, data)}
+                onStop={(e, data) =>
+                    this.onDragStop(locatedCard, data)}>
+
+                <div className={className}>
+                    {this.props.locatedCard.card.open
+                    ? this.props.locatedCard.card.name
+                    : ""}
+                </div>
+
+            </Draggable>
         );
     }
 }
--- frontend/src/TableComponent.tsx
+++ frontend/src/TableComponent.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Draggable, { DraggableData } from "react-draggable";
 import './TableComponent.css';
-import {Deck, Table, LocatedCard} from "cards-library";
+import {Deck, Table} from "cards-library";
 import CardComponent from "./CardComponent";
 import DecksComponent from "./DecksComponent";
 
@@ -11,73 +10,21 @@ type Props = {
     sendMessage: (msg: any) => void,
 };
 
-export default class TableComponent extends React.Component<Props, {}> {
-    startX: number = -1;
-    startY: number = -1;
-
-    private onCardClick(id: number) {
-        this.props.sendMessage({
-            messageType: "card_turn",
-            cardId: id,
-        });
-    }
-
-    private countAsClick(data: DraggableData) {
-        let difX = this.startX - data.x;
-        let difY = this.startY - data.y;
-        let distSq = difX * difX + difY * difY;
-        return distSq < 100;
-    }
-
-    private onDragStart(locatedCard: LocatedCard, data: DraggableData) {
-        this.startX = data.x;
-        this.startY = data.y;
-        this.props.sendMessage({
-            messageType: "card_drag",
-            cardId: locatedCard.card.id,
-            cardX: data.x,
-            cardY: data.y,
-        });
-    }
-
-    private onDragMove(locatedCard: LocatedCard, data: DraggableData) {
-        this.props.sendMessage({
-            messageType: "card_drag",
-            cardId: locatedCard.card.id,
-            cardX: data.x,
-            cardY: data.y,
-        });
-    }
-
-    private onDragStop(locatedCard: LocatedCard, data: DraggableData) {
-        if (this.countAsClick(data)) {
-            this.onCardClick(locatedCard.card.id);
-        }
-    }
+export default class TableComponent extends React.Component<Props> {
 
     public render() {
         return (
             <div className="table">
                 <DecksComponent
-                    decks={this.props.decks}></DecksComponent>
+                    decks={this.props.decks} />
+
                 {this.props.table.locatedCards.map((locatedCard) => {
-                    return <Draggable
-                               key={locatedCard.card.id}
-                               position={{
-                                   x: locatedCard.location.x,
-                                   y: locatedCard.location.y}}
-                               onStart={(e, data) =>
-                                   this.onDragStart(locatedCard, data)}
-                               onDrag={(e, data) =>
-                                   this.onDragMove(locatedCard, data)}
-                               onStop={(e, data) =>
-                                   this.onDragStop(locatedCard, data)}>
-                        <div style={{width: 0}}>
-                            <CardComponent
-                                locatedCard={locatedCard}
-                            />
-                        </div>
-                    </Draggable>;})}
-            </div>)
+                    return <CardComponent locatedCard={locatedCard}
+                                          sendMessage={this.props.sendMessage}
+                                          key={locatedCard.card.id}/>
+                    ;})}
+
+            </div>
+        );
     }
 }
