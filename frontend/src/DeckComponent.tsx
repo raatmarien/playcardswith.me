860160b3e535151d2692abf12be419a368c6a515
Marien Raat <marienraat@riseup.net>
2020-03-28 18:17:02 +0100

    Make cards draggable from deck

--- frontend/src/DeckComponent.tsx
+++ frontend/src/DeckComponent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Deck} from "cards-library";
+import {Deck, Card} from "cards-library";
 import "./DeckComponent.css";
+import {DraggableCore, DraggableData} from 'react-draggable';
 
 type Props = {
     deck: Deck,
@@ -8,7 +9,16 @@ type Props = {
     onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
 }
 
-export default class DeckComponent extends React.Component<Props, {}> {
+type State = {
+    draggingCard: Card | null;
+}
+
+export default class DeckComponent extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.setState({draggingCard: null});
+    }
+
     private recallToThisDeck() {
         this.props.sendMessage({
             messageType: "recall_to_deck",
@@ -16,16 +26,48 @@ export default class DeckComponent extends React.Component<Props, {}> {
         });
     }
 
+    private onDragStart(data: DraggableData) {
+        console.log("drag start");
+        this.props.sendMessage({
+            messageType: "pick_from_deck",
+            deckId: this.props.deck.id,
+            cardX: data.x,
+            cardY: data.y,
+        });
+        this.setState({
+            draggingCard:
+                         this.props.deck.cards[
+                             this.props.deck.cards.length-1]});
+    }
+
+    private onDragMove(data: DraggableData) {
+        this.props.sendMessage({
+            messageType: "card_drag",
+            cardX: data.x,
+            cardY: data.y,
+            cardId: this.state.draggingCard!.id,
+        });
+    }
+
+    private onDragStop(data: DraggableData) {
+
+    }
+
     public render() {
         return (
-            <div className="deck">
-                <button onClick={this.recallToThisDeck.bind(this)}>&#8942;</button>
-            <div className="inner-deck" onClick={this.props.onClick}>
-                <div>
-                    {this.props.deck.cards.length} cards
+            <DraggableCore
+                onStart={(e, data) => this.onDragStart(data)}
+                onDrag={(e, data) => this.onDragMove(data)}
+                onStop={(e, data) => this.onDragStop(data)}>
+                <div className="deck">
+                    <button onClick={this.recallToThisDeck.bind(this)}>&#8942;</button>
+                    <div className="inner-deck" onClick={this.props.onClick}>
+                        <div>
+                            {this.props.deck.cards.length} cards
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
+            </DraggableCore>
         );
     }
 }
