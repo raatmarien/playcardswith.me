6e7474fe17be95e3e130e12e2fa039c598d2c7e4
Sam Beard <sam.beard@zelezny.nl>
2020-03-28 19:17:02 +0100

    add join room component

---
+++ frontend/src/JoinRoomComponent.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './RegisterFormComponent.css';
+import './CreateRoomComponent.css';
+
+type State = {
+    roomID: string,
+}
+export default class JoinRoomComponent extends React.Component<{},State> {
+    constructor(props : any) {
+        super(props);
+
+        this.state = {roomID: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    public render() {
+        console.log(this.state.roomID);
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label> Fill in the room ID below to join a game room</label>
+                <div className="input-wrapper">
+                    <input type="text" value={this.state.roomID} onChange={this.handleChange} />
+                    <input type="submit" value="Join room" />
+                </div>
+            </form>
+        );
+    }
+    handleChange(event: any) {
+        this.setState({roomID: event.target.value});
+    }
+    handleSubmit(event: any) {
+        // TODO: go to room {this.state.roomID}   
+    }
+}
+
+
