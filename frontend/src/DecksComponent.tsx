d8202745d623cfd2c7924d44017c4ea064bc2caa
Marien Raat <marienraat@riseup.net>
2020-04-02 19:44:45 +0200

    Use react-select for multi select

--- frontend/src/DecksComponent.tsx
+++ frontend/src/DecksComponent.tsx
@@ -5,7 +5,9 @@ import "./DecksComponent.css";
 import {Button} from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
 import {CirclePicker} from "react-color";
+import Select from "react-select";
 
 type Props = {
     decks: Deck[],
@@ -15,42 +17,58 @@ type Props = {
 
 type State = {
     show: boolean,
-    includedCards: string[],
-    includedSuits: string[],
+    includedCards: any[],
+    includedSuits: any[],
     amountOfEach: number,
     deckColor: string,
     shuffleDeck: boolean,
 };
 
 export default class DecksComponent extends React.Component<Props, State> {
-    private readonly allCards = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
-                                 "J", "Q", "K", "A"];
-    private readonly allSuits = ["♠", "♥", "♦", "♣"];
+    private readonly allCardOptions = [
+        { value: "2", label: "2" },
+        { value: "3", label: "3" },
+        { value: "4", label: "4" },
+        { value: "5", label: "5" },
+        { value: "6", label: "6" },
+        { value: "7", label: "7" },
+        { value: "8", label: "8" },
+        { value: "9", label: "9" },
+        { value: "10", label: "10" },
+        { value: "J", label: "Jack" },
+        { value: "Q", label: "Queen" },
+        { value: "K", label: "King" },
+        { value: "A", label: "Ace" }]
+    private readonly allSuitOptions = [
+        { value: "♠", label: "♠ Clubs" },
+        { value: "♣", label: "♣ Clovers" },
+        { value: "♥", label: "♥ Hearts" },
+        { value: "♦", label: "♦ Diamonds" }]
     private readonly possibleColors = [
-        "#b90e0e",
-        "#b9690e",
-        "#c3b004",
-        "#83be04",
-        "#2a660a",
-        "#0bb177",
-        "#0a9e99",
-        "#0a6a9e",
-        "#081e81",
-        "#4c13be",
-        "#6d10a2",
-        "#a2109b",
-        "#a21057",
-        "#493232",
-        "#323449",
-        "#324932",
-        "#494732",
-        "#470200",
-        "#474100",
-        "#004711",
-        "#0e0047",
-        "#470047"]
+            "#b90e0e",
+            "#b9690e",
+            "#c3b004",
+            "#83be04",
+            "#2a660a",
+            "#0bb177",
+            "#0a9e99",
+            "#0a6a9e",
+            "#081e81",
+            "#4c13be",
+            "#6d10a2",
+            "#a2109b",
+            "#a21057",
+            "#493232",
+            "#323449",
+            "#324932",
+            "#494732",
+            "#470200",
+            "#474100",
+            "#004711",
+            "#0e0047",
+            "#470047"]
 
-    constructor(props: Props) {
+        constructor(props: Props) {
         super(props);
         this.state = this.defaultNewDeckState();
     }
@@ -58,8 +76,8 @@ export default class DecksComponent extends React.Component<Props, State> {
     private defaultNewDeckState() {
         return {
             show: false,
-            includedCards: this.allCards,
-            includedSuits: this.allSuits,
+            includedCards: this.allCardOptions,
+            includedSuits: this.allSuitOptions,
             amountOfEach: 1,
             deckColor: this.possibleColors[this.props.decks.length
                 % this.possibleColors.length],
@@ -68,9 +86,13 @@ export default class DecksComponent extends React.Component<Props, State> {
     }
 
     private setShow(showDialog: boolean) {
-        let state = this.defaultNewDeckState();
-        state.show = true;
-        this.setState(state);
+        if (showDialog) {
+            let state = this.defaultNewDeckState();
+            state.show = true;
+            this.setState(state);
+        } else {
+            this.setState({ show: false })
+        }
     }
 
     private getNextTotalCards() {
@@ -83,12 +105,12 @@ export default class DecksComponent extends React.Component<Props, State> {
         return [...event.target.selectedOptions].map((o: any) => o.value);
     }
 
-    private handleIncludedCardsChanged(event: any) {
-        this.setState({ includedCards: this.getOptionsFromEvent(event) });
+    private handleIncludedCardsChanged(selectedOptions: any) {
+        this.setState({ includedCards: selectedOptions });
     }
 
-    private handleIncludedSuitsChanged(event: any) {
-        this.setState({ includedSuits: this.getOptionsFromEvent(event) });
+    private handleIncludedSuitsChanged(selectedOptions: any) {
+        this.setState({ includedSuits: selectedOptions });
     }
 
     private handleAmountOfEachChanged(event: any) {
@@ -102,8 +124,8 @@ export default class DecksComponent extends React.Component<Props, State> {
     private createDeck() {
         this.props.sendMessage({
             messageType: "add_deck",
-            includedCards: this.state.includedCards,
-            includedSuits: this.state.includedSuits,
+            includedCards: this.state.includedCards.map((o) => o.value),
+            includedSuits: this.state.includedSuits.map((o) => o.value),
             amountOfEach: this.state.amountOfEach,
             deckColor: this.state.deckColor,
             shuffleDeck: this.state.shuffleDeck,
@@ -138,50 +160,41 @@ export default class DecksComponent extends React.Component<Props, State> {
                                 <Form.Label>
                                     Included cards
                                 </Form.Label>
-                                <Form.Control value={this.state.includedCards}
-                                              onChange={this.handleIncludedCardsChanged.bind(this)}
-                                              as="select" multiple>
-                                    {
-                                        this.allCards.map((c) => {
-                                            return (<option
-                                                        value={c}>
-                                                {c}
-                                            </option>);
-                                        })
-                                    }
-                                </Form.Control>
+                                <Select
+                                    id="includedCards"
+                                    value={this.state.includedCards}
+                                    options={this.allCardOptions}
+                                    onChange={this.handleIncludedCardsChanged.bind(this)}
+                                    isMulti={true} />
                             </Form.Group>
                             <Form.Group controlId="includedSuits" >
                                 <Form.Label>
                                     Included suits
                                 </Form.Label>
-                                <Form.Control value={this.state.includedSuits}
-                                              onChange={this.handleIncludedSuitsChanged.bind(this)}
-                                              as="select" multiple>
-                                    {
-                                        this.allSuits.map((s, i) => {
-                                            return (<option
-                                                        value={s}
-                                                        className={i > 1 ?
-                                                                  "red-option" : ""}
-                                                        selected>
-                                                {s}
-                                            </option>);
-                                        })
-                                    }
-                                </Form.Control>
+                                <Select
+                                    id="includedSuits"
+                                    value={this.state.includedSuits}
+                                    options={this.allSuitOptions}
+                                    onChange={this.handleIncludedSuitsChanged.bind(this)}
+                                    isMulti={true} />
                             </Form.Group>
                             <Form.Group controlId="amountOfEach" >
                                 <Form.Label>
                                     Amount of copies
                                 </Form.Label>
+                                <Form.Text className="text-muted">
+                                    
+                                </Form.Text>
                                 <Form.Control type="number"
                                               value={this.state.amountOfEach}
                                               onChange={this.handleAmountOfEachChanged.bind(this)} />
+                                <Form.Text className="text-muted">
+                                    The amount of copies of each
+                                    specific card, like the A♥. It's
+                                    like adding multiple decks of cards
+                                    into one big stack.
+                                </Form.Text>
                             </Form.Group>
-                            <Form.Label>
-                                {this.getNextTotalCards()} cards in total.
-                            </Form.Label>
                             <Form.Group controlId="deckColor" >
                                 <Form.Label>
                                     Deck color
@@ -199,6 +212,9 @@ export default class DecksComponent extends React.Component<Props, State> {
                                 label="Shuffle deck"
                                 checked={this.state.shuffleDeck}
                                 onChange={this.handleShuffleDeckChanged.bind(this)} />
+                            <Alert variant="info" className="mt-3 mb-0">
+                                {this.getNextTotalCards()} cards in total.
+                            </Alert>
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
